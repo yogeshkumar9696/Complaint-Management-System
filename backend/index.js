e7b03a88a4a113d9ce7c1a14f881d1af920b6c4d
yogeshkumar9696 <145792924+yogeshkumar9696@users.noreply.github.com>
2025-08-18 02:34:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -47,9 +47,9 @@ const __dirname= path.resolve();
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-  app.get(/^\/(?!assets\/).*/,(req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-  })
+  app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 app.listen(PORT,() => {
