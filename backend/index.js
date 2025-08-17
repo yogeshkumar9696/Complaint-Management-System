904f7b328ca90900f4f8daf7f94c6da2e1539d27
yogeshkumar9696 <145792924+yogeshkumar9696@users.noreply.github.com>
2025-08-18 02:12:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ const __dirname= path.resolve();
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-  app.get("/*",(req, res) => {
+  app.get(/.*/,(req, res) => {
     res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
   })
 }
